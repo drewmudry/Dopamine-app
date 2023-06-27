20acbfa828cdb0c3cfc7ed0c65376cfa0c337a56
drewmudry <91819824+drewmudry@users.noreply.github.com>
2023-06-26 23:35:55 -0700

    Initial Commit

---
+++ dopamine/dopamineApp.swift
@@ -0,0 +1,17 @@
+//
+//  dopamineApp.swift
+//  dopamine
+//
+//  Created by Drew on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct dopamineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
